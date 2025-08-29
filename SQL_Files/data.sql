df5eff71fee7345738adf603dacc1023cb0133ee
sai <kottunarasimha22@gmail.com>
2025-08-29 23:38:26 +0530

    Refactor: Organise SQL files into one folder

---
+++ SQL_Files/data.sql
@@ -0,0 +1,7 @@
+-- Insert into Users
+INSERT INTO users (username, password, email)
+VALUES ('sai', 'sai', 'sainaidukottu12@gmail.com');
+
+-- Insert into Customers
+INSERT INTO customers (name, email, phone)
+VALUES ('sai', 'sai@gmail.com', '9392240056');
